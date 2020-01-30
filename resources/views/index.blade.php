00713c36e4238aba56c7a4e72b77a3de59a9f471
crndolo <ndoloraphael@gmail.com>
2020-01-30 07:45:19 +0300

    Including Index page for lesmagnifique events site

---
+++ resources/views/index.blade.php
@@ -0,0 +1,399 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width initial-scale=1.0" charset="UTF-8">
+	<title>Les Magnifique</title>
+
+	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="assets/css/styles.css" type="text/css">
+</head>
+<body>
+	
+	<header class="header">
+		
+		<nav>
+			
+			<div class="logo-box">
+				<img src="assets/img/logo/sitelogo.png" alt="Logo" class="logo">
+			</div>
+			
+			<!--<div class="menu">
+				<ul>
+					<li> <a href="#"> Home </a> </li>
+					<li> <a href="#"> About Us </a> </li>
+					<li> <a href="#"> Services </a> </li>
+					<li> <a href="#"> Events </a> </li>
+					<li> <a href="#"> Contact Us </a> </li>
+				</ul>			
+			</div> -->
+			
+		</nav>
+		
+		<div class="text-box">
+
+			<h1 class="heading-primary">	
+				<span class="heading-primary-main"> Magnifique</span>	
+				<span class="heading-primary-sub"> Organizers for all your events</span>
+			</h1>
+
+			<a href="#about" class="btn btn-white"> About us </a>
+
+		</div>
+
+	</header>
+	
+	<main>
+
+		<section class="section-about">
+			<div id="about">
+				<div class="u-center-text u-margin-bottom-big">
+					<h2 class="heading-secondary">
+						About Les Magnifique
+					</h2>
+				</div>
+
+				<div class="row">
+					<div class="col-1-of-2"> 
+						<h3 class="heading-tertiary u-margin-bottom-small"> Quality of service </h3>
+						<p class="paragraph">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus nostrum aperiam, porro qui velit. Rerum fuga nisi saepe, aspernatur laboriosam ex, nostrum, suscipit beatae maiores neque quam cum, ut sapiente.
+						</p>
+
+						<h3 class="heading-tertiary u-margin-bottom-small"> All in one </h3>
+						<p class="paragraph">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus nostrum aperiam, porro qui velit. Rerum fuga nisi saepe, aspernatur laboriosam ex, nostrum, suscipit beatae maiores neque quam cum, ut sapiente.
+						</p>
+		
+						<a href="#" class="btn-text">Learn More &rarr;</a>
+
+					</div>
+
+					<div class="col-1-of-2"> 
+						
+						<div class="composition">
+							<img src="assets/img/about/image1.jpg" alt="Image One" class="composition__photo composition__photo--p1">
+							<img src="assets/img/about/image2.jpg" alt="Image Two" class="composition__photo composition__photo--p2">
+							<img src="assets/img/about/image4.jpg" alt="Image three" class="composition__photo composition__photo--p3">
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+
+		<section class="section-feature">
+			
+			<div class="row">
+
+				<div class="col-1-of-4">
+					<div class="feature-box">
+						<i class="fa fa-print feature-box__icon"></i>
+						<h3 class="heading-tertiary u-margin-bottom-small">Transcribing</h3>
+						<p class="feature-box__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium quas dolorum deleniti repudiandae voluptatibus et eius hic error!</p>
+					</div>
+				</div>
+
+				<div class="col-1-of-4">
+					<div class="feature-box">
+						<i class="fa fa-user feature-box__icon"></i>
+						<h3 class="heading-tertiary u-margin-bottom-small">Event Consultancy</h3>
+						<p class="feature-box__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium quas dolorum deleniti repudiandae voluptatibus et eius hic error!</p>
+					</div>
+				</div>
+
+				<div class="col-1-of-4">
+					<div class="feature-box">
+						<i class="fa fa-calendar feature-box__icon"></i>
+						<h3 class="heading-tertiary u-margin-bottom-small">Conferencing</h3>
+						<p class="feature-box__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium quas dolorum deleniti repudiandae voluptatibus et eius hic error!</p>
+					</div>
+				</div>
+
+				<div class="col-1-of-4">
+					<div class="feature-box">
+						<i class="fa fa-car feature-box__icon"></i>
+						<h3 class="heading-tertiary u-margin-bottom-small">Transport</h3>
+						<p class="feature-box__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium quas dolorum deleniti repudiandae voluptatibus et eius hic error!</p>
+					</div>
+				</div>
+
+			</div>
+
+		</section>
+
+		<section class="section-events">
+			
+			<div class="u-center-text u-margin-bottom-big">
+				<h2 class="heading-secondary">
+					Our recent Events
+				</h2>
+			</div>
+
+			<div class="row">
+				
+				<div class="col-1-of-3"> 
+					<div class="card">
+						<div class="card__side card__side--front">
+							
+							<div class="card__picture">
+								<img src="assets/img/events/image3.JPG" alt="Child Immunization">
+							</div>
+
+							<h4 class="card__heading">
+								Regional Animal Welfare Conference
+							</h4>
+
+							<div class="card__details">
+								<ul>
+									<li> 5 Days </li>
+									<li> 120 Delegates </li>
+									<li> 12 Nationalities </li>
+									<li> 3 Continents </li>
+								</ul>
+							</div>
+
+							<div class="hide__button">
+								<a href="" class="btn btn--white__narrow--hide"> view Event </a>
+							</div>
+
+						</div>
+
+						<div class="card__side card__side--back card__side--back-one">
+							<div class="card__cta">
+								<h4> Regional Animal Welfare Conference </h4>
+								<a href="" class="btn btn--white__narrow"> view Event </a>
+							</div>
+						</div>		
+					</div>
+				</div>
+
+				<div class="col-1-of-3"> 
+					<div class="card">
+						<div class="card__side card__side--front">
+							
+							<div class="card__picture">
+								<img src="assets/img/events/image1.JPG" alt="Child Immunization">
+							</div>
+
+							<h4 class="card__heading">
+								Child Immunization Conference
+							</h4>
+
+							<div class="card__details">
+								<ul>
+									<li> 4 Days </li>
+									<li> 220 Delegates </li>
+									<li> 23 Nationalities </li>
+									<li> 1 Continents </li>
+								</ul>
+							</div>
+
+							<div class="hide__button">
+								<a href="" class="btn btn--white__narrow--hide"> view Event </a>
+							</div>
+
+						</div>
+
+						<div class="card__side card__side--back card__side--back-one">
+							<div class="card__cta">
+								<h4> Child Immunization Conference </h4>
+								<a href="" class="btn btn--white__narrow"> view Event </a>
+							</div>
+						</div>		
+					</div>
+				</div>
+
+				<div class="col-1-of-3"> 
+					<div class="card">
+						<div class="card__side card__side--front">
+							
+							<div class="card__picture">
+								<img src="assets/img/events/image5.JPG" alt="Child Immunization">
+							</div>
+
+							<h4 class="card__heading">
+								LGBTQ Africa Health Conference
+							</h4>
+
+							<div class="card__details">
+								<ul>
+									<li> 2 Days </li>
+									<li> 320 Delegates </li>
+									<li> 15 Nationalities </li>
+									<li> 2 Continents </li>
+								</ul>
+							</div>
+
+							<div class="hide__button">
+								<a href="" class="btn btn--white__narrow--hide"> view Event </a>
+							</div>
+
+						</div>
+
+						<div class="card__side card__side--back card__side--back-one">
+							<div class="card__cta">
+								<h4> LGBTQ Africa Health Conference </h4>
+								<a href="" class="btn btn--white__narrow"> view Event </a>
+							</div>
+						</div>		
+					</div>
+				</div>
+
+			</div>
+
+			<div class="u-center-text u-margin-top-big">
+				<a href="#" class="btn btn--gold"> View All Events </a>	
+
+		</section>
+
+		<section class="section-contact">
+			<div class="row">
+				<div class="contact">
+					<img src="assets/img/contact/contact.jpg" alt="">
+
+					<div class="contact__cover"> <h2> Contact Form </h2> </div>
+
+					<div class="contact__box contact-title">
+						<form action="">
+							<div class="form__group">
+								<!--<label for="first__name" class="form__label">First Name:</label>-->
+								<input type="text" class="form__input" placeholder="First Name" id="first__name" name="first__name" required>
+							</div>
+
+							<div class="form__group">
+								<!--<label for="last__name" class="form__label">Last Name:</label>-->
+								<input type="text" class="form__input" placeholder="Last Name" id="last__name" name="last__name" >
+							</div>
+
+							<div class="form__group">
+								<!--<label for="email" class="form__label">Email:</label>-->
+								<input type="email" class="form__input" placeholder="Email" id="email" name="email" required>
+							</div>							
+					</div>
+
+					<div class="contact__message">
+							<div class="form__group">
+								<label for="message" class="form__label--message">Message:</label>
+								<textarea class="form__textarea ckeditor" name="message" id="message"></textarea>
+							</div>
+
+							<div class="form__group">
+								<input type="submit" class="form__btn btn__submit" value="Send">
+							</div>
+
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</main>
+
+	<footer class="footer">
+		
+		<div class="footer__logo-box">
+			<img src="assets/img/logo/logo.PNG" alt="Logo" class="footer__logo">
+		</div>
+
+		<div class="row">
+			
+			<div class="col-1-of-2">
+				<div class="footer__navigation">
+
+					<ul class="footer__list">
+						<li class="footer__item"><a href="" class="footer__link"> Home  </a></li>
+						<li class="footer__item"><a href="" class="footer__link"> About Us </a></li>
+						<li class="footer__item"><a href="" class="footer__link"> Services </a></li>
+						<li class="footer__item"><a href="" class="footer__link"> Events </a></li>
+						<li class="footer__item"><a href="" class="footer__link"> Contact Us </a></li>
+					</ul>
+
+				</div>
+			</div>
+			
+			<div class="col-1-of-2">
+				
+				<div class="footer__navigation social__media--links">
+					<ul class="footer__list">
+						<li class="footer__item--social">
+							<a href="" class="footer__link">
+								<i class="fa fa-facebook"></i>
+							</a>
+						</li>
+
+						<li class="footer__item--social">
+							<a href="" class="footer__link">
+								<i class="fa fa-twitter"></i>
+							</a>
+						</li>
+
+						<li class="footer__item--social">
+							<a href="" class="footer__link">
+								<i class="fa fa-instagram"></i>
+							</a>
+						</li>
+
+						<li class="footer__item--social">
+							<a href="" class="footer__link">
+								<i class="fa fa-google-plus"></i>
+							</a>
+						</li>
+						
+						<li class="footer__item--social">
+							<div class="footer__powered--by">
+								Powered by with <i class="fa fa-heart"></i> <a href="">&copy;crndolo</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+
+		</div>
+
+	</footer>
+	<!--
+	<div class="grid">
+		
+		<div class="row">
+			<div class="col-1-of-2"> 1 Of 2</div>
+			<div class="col-1-of-2"> 1 Of 2 </div>	
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-3"> 1 Of 3 </div>
+			<div class="col-1-of-3"> 1 Of 3 </div>
+			<div class="col-1-of-3"> 1 Of 3 </div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-3"> 1 Of 3 </div>
+			<div class="col-2-of-3"> 2 Of 3 </div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-4"> 1 Of 4 </div>
+			<div class="col-1-of-4"> 1 Of 4 </div>
+			<div class="col-1-of-4"> 1 Of 4 </div>
+			<div class="col-1-of-4"> 1 Of 4 </div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-4"> 1 Of 4 </div>
+			<div class="col-1-of-4"> 1 Of 4 </div>			
+			<div class="col-2-of-4"> 2 Of 4 </div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-4"> 1 Of 4 </div>
+			<div class="col-3-of-4"> 3 Of 4 </div>
+		</div>
+
+	</div>
+	-->
+
+	<script src="assets/ckeditor/ckeditor.js"></script>
+</body>
+</html>
