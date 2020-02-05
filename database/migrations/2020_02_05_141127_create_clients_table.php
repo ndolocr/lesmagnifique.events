2ea4bf31efcca82c8ccf7d615d75b822235c4690
crndolo <ndoloraphael@gmail.com>
2020-02-05 17:38:25 +0300

    Creating client migration file and model

--- database/migrations/2020_02_05_141127_create_clients_table.php
+++ database/migrations/2020_02_05_141127_create_clients_table.php
@@ -15,6 +15,13 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('area_code');
+            $table->string('town');
+            $table->string('email');
+            $table->integer('telephone');
+            $table->integer('event_id');
             $table->timestamps();
         });
     }
