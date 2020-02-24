252dee992bca6b56ea9af1273294e55fff162b0d
crndolo <ndoloraphael@gmail.com>
2020-02-25 01:20:34 +0300

    Including font awesome in index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
 	<meta name="viewport" content="width=device-width initial-scale=1.0" charset="UTF-8">
 	<title>Les Magnifique</title>
 
-	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
-	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/font-awesome/css/all.css">
+	<link rel="stylesheet" href="assets/font-awesome/css/all.min.css">
 
 	<link rel="stylesheet" href="assets/css/styles.css" type="text/css">
 </head>
