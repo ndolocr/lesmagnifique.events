eb67678c943af6a5e6efd18052ffc84a2a0298cf
crndolo <ndoloraphael@gmail.com>
2020-02-24 15:36:52 +0300

    Rename Service model to Service model. Change from plural ti singular.

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Event;
+use App\Service;
 use Illuminate\Http\Request;
 
 class ServicesController extends Controller
@@ -14,7 +14,7 @@ class ServicesController extends Controller
      */
     public function index()
     {
-        $events = Event::orderBy('created_at')->paginate(6);
+        $events = Service::orderBy('created_at')->paginate(6);
 
         return view('admin.services-all', compact('events'));
     }
