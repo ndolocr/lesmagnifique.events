0e3ff2ec2f16e3e8b89a0102d6313e454685eda2
crndolo <ndoloraphael@gmail.com>
2020-03-19 08:39:14 +0300

    Creating and styling About us header

--- resources/views/front-end/about.blade.php
+++ resources/views/front-end/about.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.frontEndMaster')
 
 @section('content')
-	About Page
+	<header class="page-header">
+		
+		<img src="assets/img/contact/contact.jpg" alt="">
+
+		<!-- BEGIN HEADER COVER -->
+		<div class="page-header__cover">
+	
+		</div>
+		<!-- END HEADER COVER -->
+
+		<!-- BEGIN NAV SECTION -->
+		<nav>
+			<a href="{{ route('index') }}">
+				<div class="logo-box"> <img src="assets/img/logo/sitelogo.png" alt="Logo" class="logo"> </div>
+			</a>
+		</nav>
+		<!-- END NAV SECTION -->
+
+		<!-- BEGIN ABOUT US HEADER -->
+		<div class="heading">
+			About Us
+		</div>
+		<!-- END ABOUT US HEADER -->
+	</header>
 @endsection
