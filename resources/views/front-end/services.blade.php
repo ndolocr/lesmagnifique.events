73c85611235919bdf4071239f4af3cbd0077d2f0
crndolo <ndoloraphael@gmail.com>
2020-03-24 18:12:19 +0300

    Changing page design for events page

--- resources/views/front-end/services.blade.php
+++ resources/views/front-end/services.blade.php
@@ -1,43 +1,6 @@
 @extends('layouts.singlePageMaster')
 
 @section('content')
-	<!-- BEGIN CONTAINER -->
-	<div class="container-fluid">
-		<!-- BEGIN ROW -->
-		<div class="row">	
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">	
-				<header class="header-page">
-
-					<!-- BEGIN CLASS FOR COVER IMAGE -->
-					<div class="cover-image"> <img src="storage/assets/img/homepage/1584683134.jpg" alt=""> </div>
-					<!-- END CLASS FOR COVER IMAGE -->
-
-					<!-- BEGIN CLASS FOR COVER GRADIENT -->
-					<div class="cover-gradient"> </div>
-					<!-- END CLASS FOR COVER GRADIENT -->
-					
-					<!-- BEGIN LOGO SECTION -->
-					<div class="logo">	
-						<!-- BEGIN NAV SECTION -->
-						<nav>
-							<a href="{{ route('index') }}">
-								<div class="logo-box"> <img src="assets/img/logo/sitelogo.png" alt="Logo" class="logo"> </div>
-							</a>
-						</nav>
-						<!-- END NAV SECTION -->
-					</div>
-					<!-- END LOGO SECTION -->
-
-					<div class="header-title">
-						Services
-					</div>
-
-				</header>
-			</div>
-		</div>
-		<!-- END ROW -->
-	</div>
-	<!-- END CONTAINER -->
 
 	<!-- BEGIN CONTAINER -->
 	<div class="container-fluid">
--- resources/views/layouts/singlePageMaster.blade.php
+++ resources/views/layouts/singlePageMaster.blade.php
@@ -11,8 +11,51 @@
 		<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/singlePageStyles.css') }}">
 	</head>
 
-	<body> 
-		@yield('content') 
+	<body>
+		<!-- BEGIN CONTAINER -->
+		<div class="container-fluid">
+			<!-- BEGIN ROW -->
+			<div class="row">	
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">	
+					<header class="header-page">
+
+						<!-- BEGIN CLASS FOR COVER IMAGE -->
+						<div class="cover-image"> <img src="storage/assets/img/homepage/1584683134.jpg" alt=""> </div>
+						<!-- END CLASS FOR COVER IMAGE -->
+
+						<!-- BEGIN CLASS FOR COVER GRADIENT -->
+						<div class="cover-gradient"> </div>
+						<!-- END CLASS FOR COVER GRADIENT -->
+					
+						<!-- BEGIN LOGO SECTION -->
+						<div class="logo">	
+							<!-- BEGIN NAV SECTION -->
+							<nav>
+								<a href="{{ route('index') }}">
+									<div class="logo-box"> <img src="assets/img/logo/sitelogo.png" alt="Logo" class="logo"> </div>
+								</a>
+							</nav>
+							<!-- END NAV SECTION -->
+						</div>
+						<!-- END LOGO SECTION -->
+
+						<div class="header-title">
+							@yield('page-title')
+						</div>
+
+					</header>
+				</div>
+			</div>
+			<!-- END ROW -->
+		</div>
+		<!-- END CONTAINER -->
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+				@yield('content') 		
+			</div>
+		</div>
+		
 		
 		<footer class="footer">
 		
